b5871efeae1978a2b1ec7e786268b25d6c74cd99
Porter Provan <pprovan@wisc.edu>
2026-02-24 11:47:46 -0600

    I am updating the test file structure and SpringBootTest. I also updated the SpringBoot app.

--- BoggleSpringBoot/src/main/java/com/bogglespringboot/BoggleApplication.java
+++ BoggleSpringBoot/src/main/java/com/bogglespringboot/BoggleApplication.java
@@ -1,4 +1,4 @@
-package com.example.Boggle;
+package com.bogglespringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoggleApplication {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(BoggleApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BoggleApplication.class, args);
+    }
 }
