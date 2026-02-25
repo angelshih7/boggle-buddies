f40a3b8710956024ef3b2a96630cd6a0c767a057
raudales <raudales@wisc.edu>
2026-02-25 12:47:36 -0600

    added entities Board, Dictionary and FoundWord

--- BoggleSpringBoot/src/main/java/com/bogglespringboot/Model/Tables/Board.java
+++ BoggleSpringBoot/src/main/java/com/bogglespringboot/Model/Tables/Board.java
@@ -9,20 +9,20 @@ import java.time.LocalDateTime;
 @Table(name = "boards")
 public class Board {
     @Id
-    @Column(name = "board_id",length = 50)
-    private String boarId;
+    @Column(name = "board_id",length = 50, nullable = false)
+    private String boardId;
 
     //Stores board letters and layout
     @Column(name="board_string",columnDefinition = "text", nullable = false)
     private String boardString;
 
-    @Column(name="created_at")
+    @Column(name="created_at",nullable = false,updatable = false)
     private LocalDateTime createdAt;
 
     public Board(){}
 
-    public String getBoardId() { return boarId; }
-   public void setBoardId(String boardId) { this.boarId = boardId; }
+    public String getBoardId() { return boardId; }
+   public void setBoardId(String boardId) { this.boardId = boardId; }
 
     public String getBoardString() { return boardString; }
     public void setBoardString(String boardString) { this.boardString = boardString; }
