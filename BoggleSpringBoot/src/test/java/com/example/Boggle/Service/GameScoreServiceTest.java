79c5eb8ac12c21403cdb200d8d357b045ac053cb
ashih5 <ashih5@wisc.edu>
2026-03-18 22:03:11 -0500

    fixed GameScoreServiceTest tests

--- BoggleSpringBoot/src/test/java/com/example/Boggle/Service/GameScoreServiceTest.java
+++ BoggleSpringBoot/src/test/java/com/example/Boggle/Service/GameScoreServiceTest.java
@@ -10,8 +10,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.DisplayName;
+
 
 import java.util.Optional;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
