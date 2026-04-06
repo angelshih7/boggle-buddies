2876f6791ba366dd8343b3fd525d17eb17602ca3
raudales <raudales@wisc.edu>
2026-04-06 10:50:05 -0500

    added fix for pipeline to guest logic

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -121,6 +121,7 @@ export default function GamePage() {
 
   // ---- Word submission --------------------------------------------------
 
+  // eslint-disable-next-line react-compiler/react-compiler, react-hooks/exhaustive-deps
   const submitWord = useCallback(async (word) => {
     if (word.length < MIN_WORD_LENGTH) {
       setFeedback({ word, accepted: false, reason: 'TOO_SHORT' });
